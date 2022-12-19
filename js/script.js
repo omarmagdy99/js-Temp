45a60f3814d2d86c9b6eb4c163169efd1b01a539
omar magdy abdelmoneam <47141593+omarmagdy99@users.noreply.github.com>
2022-12-19 13:08:44 +0200

    color box

--- js/script.js
+++ js/script.js
@@ -13,6 +13,27 @@ setInterval(() => {
   //   console.log(randomImage);
   landingPage.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("pic/${landingImage[randomImage]}")`;
 }, 8000);
+// set array Image
+
+
+
+
+
+
+
+// nave bar open button
+
+let navShow = document.querySelector(".landing-page .header-area .nav-show");
+let navLayout = document.querySelector(".nav-layout");
+navShow.addEventListener("click", () => {
+  settingList.classList.remove("setting-box-open");
+  navLayout.style.display = "block";
+  navbarLinks.style.right = "0";
+});
+// nave bar open button
+
+
+
 
 // nave bar close button
 let closeNav = document.querySelector(
@@ -21,10 +42,68 @@ let closeNav = document.querySelector(
 let navbarLinks = document.querySelector(".landing-page .header-area .links");
 
 closeNav.addEventListener("click", function (e) {
+  navLayout.style.display = "none";
   navbarLinks.style.right = "-100vw";
 });
+// nave bar close button
 
-let navShow = document.querySelector(".landing-page .header-area .nav-show");
-navShow.addEventListener("click", () => {
-  navbarLinks.style.right = "0";
+
+
+
+
+
+
+
+// setting box
+
+
+
+
+
+// button show
+let settingButton = document.querySelector(".setting-box .setting-icon");
+let settingList = document.querySelector(".setting-box");
+settingButton.addEventListener("click", () => {
+  settingList.classList.toggle("setting-box-open");
+  document
+    .querySelector(".setting-box .setting-icon .fa-gear")
+    .classList.toggle("fa-spin");
 });
+// button show
+
+
+
+
+
+
+
+
+// color setting
+let dataColor = document.querySelectorAll(".setting-body .color li");
+dataColor.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    dataColor.forEach((liActive) => {
+      liActive.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    let mainColor = li.getAttribute("data-color");
+    document.documentElement.style.setProperty("--main-color", mainColor);
+    localStorage.setItem("color-storage",mainColor);
+  });
+});
+// color setting
+
+
+
+
+//check local storage color
+colorStorage=localStorage.getItem("color-storage");
+if(colorStorage!==null){
+  document.documentElement.style.setProperty("--main-color", colorStorage);
+  dataColor.forEach((liActive) => {
+    liActive.classList.remove("active");
+  });
+  document.querySelector(`li[data-color='${colorStorage}']`).classList.add("active");
+}
+//check local storage color
+// setting box
