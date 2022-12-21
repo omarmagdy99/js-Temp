c54c70cd14ff467b534434ef2eb74862c781c591
omar magdy abdelmoneam <47141593+omarmagdy99@users.noreply.github.com>
2022-12-21 07:52:18 +0200

    random image

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-// get landing page
-let landingPage = document.querySelector(".landing-page");
-// set array Image
-let landingImage = [
-  "R.jfif",
-  "R (1).jfif",
-  "R (2).jfif",
-  "R (3).jfif",
-  "R (4).jfif",
-];
-setInterval(() => {
-  let randomImage = Math.floor(Math.random() * landingImage.length);
-  //   console.log(randomImage);
-  landingPage.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("pic/${landingImage[randomImage]}")`;
-}, 8000);
-// set array Image
-
-
-
-
-
-
+function randomSetImage() {
+  // get landing page
+  let landingPage = document.querySelector(".landing-page");
+  // set array Image
+  let landingImage = [
+    "R.jfif",
+    "R (1).jfif",
+    "R (2).jfif",
+    "R (3).jfif",
+    "R (4).jfif",
+  ];
+  intervalRandom = setInterval(() => {
+    let randomImage = Math.floor(Math.random() * landingImage.length);
+    //   console.log(randomImage);
+    landingPage.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url("pic/${landingImage[randomImage]}")`;
+  }, 8000);
+  // set array Image
+}
 
 // nave bar open button
 
@@ -31,9 +27,6 @@ navShow.addEventListener("click", () => {
   navbarLinks.style.right = "0";
 });
 // nave bar open button
-
-
-
 
 // nave bar close button
 let closeNav = document.querySelector(
@@ -47,18 +40,7 @@ closeNav.addEventListener("click", function (e) {
 });
 // nave bar close button
 
-
-
-
-
-
-
-
 // setting box
-
-
-
-
 
 // button show
 let settingButton = document.querySelector(".setting-box .setting-icon");
@@ -71,13 +53,6 @@ settingButton.addEventListener("click", () => {
 });
 // button show
 
-
-
-
-
-
-
-
 // color setting
 let dataColor = document.querySelectorAll(".setting-body .color li");
 dataColor.forEach((li) => {
@@ -88,22 +63,59 @@ dataColor.forEach((li) => {
     e.target.classList.add("active");
     let mainColor = li.getAttribute("data-color");
     document.documentElement.style.setProperty("--main-color", mainColor);
-    localStorage.setItem("color-storage",mainColor);
+    localStorage.setItem("color-storage", mainColor);
   });
 });
 // color setting
 
-
-
-
 //check local storage color
-colorStorage=localStorage.getItem("color-storage");
-if(colorStorage!==null){
+colorStorage = localStorage.getItem("color-storage");
+if (colorStorage !== null) {
   document.documentElement.style.setProperty("--main-color", colorStorage);
   dataColor.forEach((liActive) => {
     liActive.classList.remove("active");
   });
-  document.querySelector(`li[data-color='${colorStorage}']`).classList.add("active");
+  document
+    .querySelector(`li[data-color='${colorStorage}']`)
+    .classList.add("active");
 }
 //check local storage color
+
+// random image ask button
+randomAskButton = document.querySelectorAll(
+  ".setting-body .random-image-box .r-image-div button"
+);
+randomAskButton.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    randomAskButton.forEach((btnRC) => {
+      btnRC.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    localStorage.setItem("randomImage", e.target.getAttribute("data-random"));
+    if (e.target.getAttribute("data-random") === "yes") {
+      randomSetImage();
+    } else {
+      clearInterval(intervalRandom);
+    }
+  });
+});
+
+// set random image by local storage
+if (
+  localStorage.getItem("randomImage") === null ||
+  localStorage.getItem("randomImage") === "yes"
+) {
+  randomSetImage();
+  document
+    .querySelector(".setting-body .random-image-box .r-image-div .yes")
+    .classList.add("active");
+} else {
+  document
+    .querySelector(".setting-body .random-image-box .r-image-div .no")
+    .classList.add("active");
+}
+
+// set random image by local storage
+
+// random image ask button
 // setting box
